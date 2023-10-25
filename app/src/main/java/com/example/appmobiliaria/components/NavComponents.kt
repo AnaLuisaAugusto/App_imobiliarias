6137d90d3197980f8baffa1a6acad4a95cbbf412
AnaLuisaAugusto <ana.val01@etec.sp.gov.br>
2023-10-25 10:46:02 -0300

    first commit

---
+++ app/src/main/java/com/example/appmobiliaria/components/NavComponents.kt
@@ -0,0 +1,41 @@
+package com.example.appmobiliaria.components
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NavContent() {
+    Row(
+        modifier = Modifier.fillMaxWidth().padding(end = 10.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+
+    ){
+        Text(
+            text = "Ana Luísa Augusto",
+            color = Color.White
+        )
+        Icon(
+            imageVector = Icons.Filled.Search,
+            contentDescription = "Icon",
+            tint = Color.White,
+            modifier = Modifier.size(40.dp)
+        )
+    }
+}
